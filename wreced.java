b88a4f74ad3ad8acb57f437ed9770c0d49eee737
Michael Ward <gecotron@gmail.com>
2023-04-26 11:10:27 -0400

    more rectangle

--- wreced.java
+++ wreced.java
@@ -3,18 +3,18 @@ class Rectangle {
     private int bottom;
     private int height;
     private int width;
-
+    // Base initialise
     public Rectangle() {
         left = 0;
         bottom = 0;
         height = 0;
         width = 0;
     }
-
+    // Basic constructor
     public Rectangle(int l, int b, int w, int h) {
         this.set(l, b, w, h);
     }
-
+    // Set Rectangle values
     public void set(int l, int b, int w, int h) {
         left = l;
         bottom = b;
@@ -31,49 +31,42 @@ class Rectangle {
     public String toString() {
         return "base: (" + left + "," + bottom + ") w:" + width + " h:" + height;
     }
-
+    // return the area of the rectangle
     public int area() {
         return (height * width);
     }
-
+    // Return Perimeter of the rectangle
     public int perimeter() {
         if (width == 0) {
             return height;
         } else if (height == 0) {
             return width;
-        } else if (width == 0 && height == 0) {
-            return 0;
         } else {
             return 2 * (height + width);
         }
     }
-
+    // Check if the rectangle contains another rectangle
     public boolean contains(Rectangle rect) {
-        boolean contained;
-        if (rect.left >= this.left && rect.bottom >= this.bottom) {
-            if (rect.height >= this.height && rect.width >= this.width) {
-                contained = true;
-            } else
-                contained = false;
-        } else
-            contained = false;
-
-        return contained;
+        if (rect.left >= this.left && rect.bottom >= this.bottom && (rect.left + rect.width) <= (this.left + this.width) &&  (rect.bottom + rect.height) <= (this.bottom + this.height)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-
+    // Check if a rectangle intersects with another rectangle
     public boolean intersect(Rectangle rect) {
         return this.left < rect.left + rect.width && this.left + this.width > rect.left
                 && this.bottom < rect.bottom + rect.height && this.bottom + this.height > rect.bottom;
 
     }
-
+    // create a third 
     public static Rectangle intersection(Rectangle rectA, Rectangle rectB) {
         return new Rectangle(Math.max(rectA.left, rectB.left), Math.max(rectA.bottom, rectB.bottom),
                 (Math.min(rectA.left + rectA.width, rectB.left + rectB.width) - Math.max(rectA.left, rectB.left)),
                 (Math.min(rectA.bottom + rectA.height, rectB.bottom + rectB.height) - Math.max(rectA.bottom, rectB.bottom)));
     }
 
-    public static int totalperimeter(Rectangle rectA, Rectangle rectB) {
+    public static int totalPerimeter(Rectangle rectA, Rectangle rectB) {
         if (!rectA.contains(rectB) && rectA.intersect(rectB)) {
             Rectangle rectC = intersection(rectA, rectB);
             return (rectA.height + rectB.height - rectC.height) * 2 + (rectA.width + rectB.width - rectC.width) * 2;
