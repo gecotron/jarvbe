cb431125e040848e8908feae7b477c56a0ba4daa
Michael Ward <gecotron@gmail.com>
2023-04-28 10:12:18 -0400

    better documention (part 1 out of ???)

--- wreced.java
+++ wreced.java
@@ -1,8 +1,11 @@
 public class wreced {
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle(-7, 0, 6, 5);
+        Rectangle r1 = new Rectangle(0, -5, 6, 5);
         Rectangle r2 = new Rectangle(-1, 0, 6, 4);
-        System.out.println(Rectangle.intersection(r1, r2).toString());
+        System.out.println(r1.perimeter());
+        System.out.println(r2.perimeter());
+        System.out.println(Rectangle.intersection(r1, r2).perimeter());
+        System.out.println(Rectangle.totalPerimeter(r1, r2));
     }
 }
 
@@ -11,17 +14,20 @@ class Rectangle {
     private int bottom;
     private int height;
     private int width;
-    // Base initialise
+
+    // Base initialisation
     public Rectangle() {
         left = 0;
         bottom = 0;
         height = 0;
         width = 0;
     }
+
     // Basic constructor
     public Rectangle(int l, int b, int w, int h) {
         this.set(l, b, w, h);
     }
+
     // Set Rectangle values
     public void set(int l, int b, int w, int h) {
         left = l;
@@ -35,56 +41,78 @@ class Rectangle {
         else
             width = w;
     }
-
+    // Prints readable output of rectangle
     public String toString() {
         return "base: (" + left + "," + bottom + ") w:" + width + " h:" + height;
     }
+
     // return the area of the rectangle
     public int area() {
         return (height * width);
     }
+
     // Return Perimeter of the rectangle
     public int perimeter() {
+        // Check if the rectangle is just a line
         if (width == 0) {
             return height;
         } else if (height == 0) {
             return width;
-        } else {
+        }
+        // Otherwise return normal perimetre
+        else {
             return 2 * (height + width);
         }
     }
+
     // Check if the rectangle contains another rectangle
     public boolean contains(Rectangle rect) {
-        if (rect.left >= this.left && rect.bottom >= this.bottom && (rect.left + rect.width) <= (this.left + this.width) &&  (rect.bottom + rect.height) <= (this.bottom + this.height)) {
+        if (rect.left >= this.left && rect.bottom >= this.bottom && (rect.left + rect.width) <= (this.left + this.width)
+                && (rect.bottom + rect.height) <= (this.bottom + this.height)) {
             return true;
         } else {
             return false;
         }
     }
+
     // Check if a rectangle intersects with another rectangle
     public boolean intersect(Rectangle rect) {
         return this.left <= rect.left + rect.width && this.left + this.width >= rect.left
                 && this.bottom <= rect.bottom + rect.height && this.bottom + this.height >= rect.bottom;
     }
-    // create a new rectangle based on the intersection of 2 rectangles
+
+    // Create a new rectangle based on the intersection of 2 rectangles
     public static Rectangle intersection(Rectangle rectA, Rectangle rectB) {
+        // Checks if the rectangles actually intersect
         if (rectA.intersect(rectB) && rectB.intersect(rectA)) {
+            // Builds Rectangle
             return new Rectangle(Math.max(rectA.left, rectB.left), Math.max(rectA.bottom, rectB.bottom),
-                (Math.min(rectA.left + rectA.width, rectB.left + rectB.width) - Math.max(rectA.left, rectB.left)),
-                (Math.min(rectA.bottom + rectA.height, rectB.bottom + rectB.height) - Math.max(rectA.bottom, rectB.bottom)));
-        }
-        else {
+                    (Math.min(rectA.left + rectA.width, rectB.left + rectB.width) - Math.max(rectA.left, rectB.left)),
+                    (Math.min(rectA.bottom + rectA.height, rectB.bottom + rectB.height)
+                            - Math.max(rectA.bottom, rectB.bottom)));
+        } else {
+            // Returns empty rectangle if the given rectangles don't intersect
             return new Rectangle();
         }
     }
+
     // Get total perimetre of 2 rectangles
     public static int totalPerimeter(Rectangle rectA, Rectangle rectB) {
-        if (!rectA.contains(rectB) && rectA.intersect(rectB)) {
+        // Check if the rectangles intersect, and that one reactangle doesn't contain the other
+        if (!rectA.contains(rectB) && rectA.intersect(rectB) || !rectB.contains(rectA) && rectB.intersect(rectA)) {
+            // Build new rectangle based on intersection
             Rectangle rectC = intersection(rectA, rectB);
-            return (rectA.height + rectB.height - rectC.height) * 2 + (rectA.width + rectB.width - rectC.width) * 2;
-        } else if (rectA.contains(rectB)) {
+            // Gets total perimetre of the two rectangles, accounting for the intersected space
+            return rectA.perimeter() + rectB.perimeter() - rectC.perimeter();
+        } 
+        // If rectangle contains other rectangle, return containing rectangles perimetre
+        else if (rectA.contains(rectB)) {
             return rectA.perimeter();
-        } else {
+        } else if (rectB.contains(rectA)) {
+            return rectB.perimeter();
+        }
+        // All else fails, return the two perimetres added together
+         else {
             return rectA.perimeter() + rectB.perimeter();
         }
     }
